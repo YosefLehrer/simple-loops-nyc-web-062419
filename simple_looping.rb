f986e7595b54f8283e6c66b633c9c6c9ae3c2ab2
Yosef Lehrer <yolehrer@gmail.com>
2019-06-05 16:27:17 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -31,6 +31,7 @@ def until_iterator(number_of_times)
  until counter == 7
   puts "Welcome to Flatiron School's Web Development Course!"
   counter =+ 1 
+  end
 end
 
 def for_iterator(number_of_times)
